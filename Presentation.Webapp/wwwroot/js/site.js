16d0cac424afb82ff50288a4a5a44615d9988522
Alnils <Albin.nilsson.se@gmail.com>
2026-05-09 14:24:17 +0200

    fixed accordion behaviour

--- Presentation.Webapp/wwwroot/js/site.js
+++ Presentation.Webapp/wwwroot/js/site.js
@@ -1,4 +1,25 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(function() {
+    var $headers = $('.accordion-header');
+    var $contents = $('.accordion-content');
+    
+    if ($headers.length) {
+        $headers.first().addClass('active');
+        $contents.first().show();
+    }
+    
+    $headers.on('click', function() {
+        var $this = $(this);
+        var isActive = $this.hasClass('active');
+        
+        $headers.removeClass('active');
+        $contents.stop(true, true).slideUp(300);
+        
+        if (!isActive) {
+            $this.addClass('active');
+            $this.next('.accordion-content').stop(true, true).slideDown(300);
+        }
+    });
+});
