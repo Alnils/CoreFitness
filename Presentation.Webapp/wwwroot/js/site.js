6485b70edb7d31b2edbed5b21d150e59cfd11ce7
Alnils <Albin.nilsson.se@gmail.com>
2026-05-10 19:07:19 +0200

    fixed styling

--- Presentation.Webapp/wwwroot/js/site.js
+++ Presentation.Webapp/wwwroot/js/site.js
@@ -1,6 +1,15 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+window.updateProfileImageUrl = function(input) {
+    if (input.files && input.files.length > 0) {
+        var textInput = input.closest('.file-input-group').querySelector('.form-input');
+        if (textInput) {
+            textInput.value = input.files[0].name;
+        }
+    }
+};
+
 $(function() {
     var $headers = $('.accordion-header');
     var $contents = $('.accordion-content');
