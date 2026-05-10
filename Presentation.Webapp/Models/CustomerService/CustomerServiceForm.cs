2111b674dca0ccf7b85fb7fc389a1676b7f41516
Alnils <Albin.nilsson.se@gmail.com>
2026-05-10 17:58:05 +0200

    created customer service form

---
+++ Presentation.Webapp/Models/CustomerService/CustomerServiceForm.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Webapp.Models.CustomerService;
+
+public class CustomerServiceForm
+{
+    [Required(ErrorMessage = "First name is required.")]
+    [Display(Name = "First name", Prompt = "Enter first name")]
+
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [Display(Name = "Last name", Prompt = "Enter last name")]
+
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required.")]
+    [Display(Name = "Email", Prompt = "Enter Email Address")]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Phone]
+    [Display(Name = "Phone number", Prompt = "Enter phone number")]
+    public string? PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Message is required.")]
+    [Display(Name = "Message", Prompt = "Message...")]
+    public string Message { get; set; } = null!;
+}
